35f8df8d28bdb9be4a8c39f152515e0c29619792
Yuuki <yuuki233233@Outlook.com>
2025-11-24 22:05:49 +0800

    add

--- 练习代码记录/25_11_23/11_23.cpp
+++ 练习代码记录/25_11_23/11_23.cpp
@@ -179,31 +179,48 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//using namespace std;
+//
+//void f(int x)
+//{
+//	cout << "f(int x)" << endl;
+//}
+//
+//void f(int* ptr)
+//{
+//	cout << "f(int* ptr)" << endl;
+//}
+//
+//int main()
+//{
+//	f(0);//f(int x)
+//
+//	//本想通过f(NULL)调用指针版本的f(int*)函数，但是由于NULL被定义成0，
+//	f(NULL);//f(int x)
+//
+//	f((int*)NULL);//f(int* ptr)
+//
+//	//error:2个重载中没有一个可以转换所有参数类型
+//	//f((void*)NULL);
+//
+//	f(nullptr); //f(int* ptr)
+//	return 0;
+//}
+
 #include<iostream>
 using namespace std;
 
-void f(int x)
+class Stack
 {
-	cout << "f(int x)" << endl;
-}
+	void Push(int x)
+	{ }
+	void Pop()
+	{ }
+	int Top()
+	{ }
 
-void f(int* ptr)
-{
-	cout << "f(int* ptr)" << endl;
-}
-
-int main()
-{
-	f(0);//f(int x)
-
-	//本想通过f(NULL)调用指针版本的f(int*)函数，但是由于NULL被定义成0，
-	f(NULL);//f(int x)
-
-	f((int*)NULL);//f(int* ptr)
-
-	//error:2个重载中没有一个可以转换所有参数类型
-	//f((void*)NULL);
-
-	f(nullptr); //f(int* ptr)
-	return 0;
-}
+	int* a;
+	int top;
+	int capacity;
+};
