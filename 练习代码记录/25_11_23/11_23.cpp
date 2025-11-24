4c44fe43ba5eddbb97d5df98dbfd02d86c08cf1b
Yuuki <yuuki233233@Outlook.com>
2025-11-24 16:52:28 +0800

    添加代码

--- 练习代码记录/25_11_23/11_23.cpp
+++ 练习代码记录/25_11_23/11_23.cpp
@@ -1,6 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-using namespace std;
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
 
 //int main()
 //{
@@ -94,20 +94,30 @@ using namespace std;
 //	return 0;
 //}
 
+//int main()
+//{
+//	int a = 10;
+//
+//	const int& ra = 30;
+//
+//	//编译报错：无法从int转换为int&
+//	//int& rb = a * 3;
+//	const int& rb = a * 3;//因为a*3要创建临时对象
+//
+//	double d = 12.34;
+//	//编译报错：无法从int转换为int&
+//	//int& rd = d;
+//	const int& rd = d;//因为类型的转变，需要创建临时变量
+//
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+
 int main()
 {
-	int a = 10;
 
-	const int& ra = 30;
-
-	//编译报错：无法从int转换为int&
-	//int& rb = a * 3;
-	const int& rb = a * 3;//因为a*3要创建临时对象
-
-	double d = 12.34;
-	//编译报错：无法从int转换为int&
-	//int& rd = d;
-	const int& rd = d;//因为类型的转变，需要创建临时变量
 
 	return 0;
 }
