90431a01db4036cafa6f76bcb4f263d2a7067fe3
Yuuki <yuuki233233@Outlook.com>
2025-11-25 23:12:41 +0800

    添加练习代码

--- 练习代码记录/25_11_25/11_25.cpp
+++ 练习代码记录/25_11_25/11_25.cpp
@@ -234,59 +234,100 @@ using namespace std;
 //	return 0;
 //}
 
-class Date
+//class Date
+//{
+//public:
+//	// 1.无参构造函数
+//	Date()
+//	{
+//		_year = 1;
+//		_month = 1;
+//		_day = 1;
+//	}
+//
+//	// 2.带参构造函数
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	////3. 全缺省构造函数
+//	//Date(int year = 1, int month = 1, int day = 1)
+//	//{
+//	//	_year = year;
+//	//	_month = month;
+//	//	_day = day;
+//	//}
+//
+//	void Print()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	//如果留下三个构造中的第二个带参构造，第一个和第三个注释掉
+//
+//	Date d1; // 调用默认构造函数
+//	d1.Print(); // 输出：1/1/1
+//
+//	Date d2(2025, 11, 25); // 调用带参的构造函数
+//	d2.Print();
+//
+//	//注意：如果通过无参构造函数创建对象时，对象后面不用跟括号，否则编译器无法
+//	//区分这里是函数声明还是实例化对象
+//
+//	/*Date d3(2007, 6, 18);
+//	d3.Print();*/
+//
+//	return 0;
+//}
+
+typedef int STDataType;
+class Stack
 {
 public:
-	// 1.无参构造函数
-	Date()
+	Stack(int n = 4)
 	{
-		_year = 1;
-		_month = 1;
-		_day = 1;
-	}
+		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+		if (nullptr == _a)
+		{
+			perror("malloc fail");
+			return;
+		}
 
-	// 2.带参构造函数
-	Date(int year, int month, int day)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}
-
-	////3. 全缺省构造函数
-	//Date(int year = 1, int month = 1, int day = 1)
-	//{
-	//	_year = year;
-	//	_month = month;
-	//	_day = day;
-	//}
-
-	void Print()
-	{
-		cout << _year << "/" << _month << "/" << _day << endl;
+		_capacity = n;
+		_top = 0;
 	}
 
 private:
-	int _year;
-	int _month;
-	int _day;
+	STDataType* _a;
+	size_t _capacity;
+	size_t _top;
+};
+
+// 两个Stack实现队列
+class MyQueue
+{
+public:
+	//编译器默认生成MyQueue的构造函数调用了Stack的构造，完成了两个成员的初始化
+
+private:
+	Stack pushst;
+	Stack popst;
 };
 
 int main()
 {
-	//如果留下三个构造中的第二个带参构造，第一个和第三个注释掉
-
-	Date d1; // 调用默认构造函数
-	d1.Print(); // 输出：1/1/1
-
-	Date d2(2025, 11, 25); // 调用带参的构造函数
-	d2.Print();
-
-	//注意：如果通过无参构造函数创建对象时，对象后面不用跟括号，否则编译器无法
-	//区分这里是函数声明还是实例化对象
-
-	/*Date d3(2007, 6, 18);
-	d3.Print();*/
+	MyQueue mq;
 
 	return 0;
 }
