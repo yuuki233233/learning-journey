180c5b7d1051855b66a8dce2d32ae3c1d77171f2
Yuuki <yuuki233233@Outlook.com>
2025-11-28 16:31:30 +0800

    练习代码

--- 练习代码记录/日期/Date.cpp
+++ 练习代码记录/日期/Date.cpp
@@ -1,8 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"Date.h"
 
+bool Date::CheckDate()
+{
+	if (_month < 1 || _month > 12
+		|| _day < 1 || _day > GetMonthDay(_year, _month))
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+
 Date& Date::operator+=(int day)
 {
+	if (day < 0)
+	{
+		return *this -= (-day);
+	}
+
 	_day += day;
 	while (_day > GetMonthDay(_year, _month))
 	{
@@ -29,30 +48,23 @@ Date Date::operator+(int day)
 
 Date& Date::operator-=(int day)
 {
-	_day -= day;
-	while(_day < GetMonthDay(_year, _month))
+	if (day < 0)
 	{
-		_day += GetMonthDay(_year, _month);
+		return *this += (-day);
+	}
+
+	_day -= day;
+	while(_day <= 0)
+	{
 		_month--;
 		if (_month == 0)
 		{
 			_year--;
 			_month = 12;
 		}
+		_day += GetMonthDay(_year, _month);
 	}
 	
-	while (_day > GetMonthDay(_year, _month))
-	{
-		_day -= GetMonthDay(_year, _month);
-		_month++;
-
-		if (_month == 13)
-		{
-			_year++;
-			_month = 1;
-		}
-	}
-
 	return *this;
 }
 
@@ -62,4 +74,133 @@ Date Date::operator-(int day)
 	tmp -= day;
 
 	return tmp;
+}
+
+//Date& Date::operator-=(int day)
+//{
+//	*this = *this - day;
+//
+//	return *this;
+//}
+//
+//Date Date::operator-(int day)
+//{
+//	Date tmp = *this;
+//	
+//	tmp._day -= day;
+//	while (tmp._day <= 0)
+//	{
+//		tmp._month--;
+//		if (tmp._month == 0)
+//		{
+//			tmp._year--;
+//			tmp._month = 12;
+//		}
+//		tmp._day += GetMonthDay(tmp._year, tmp._month);
+//	}
+//
+//	return tmp;
+//}
+
+//d2 < d1
+bool Date::operator<(const Date& d)
+{
+	if (_year < d._year)
+	{
+		return true;
+	}
+	else if (_year == d._year)
+	{
+		if (_month < d._month)
+		{
+			return true;
+		}
+		else if (_month == d._month)
+		{
+			return _day < d._day;
+		}
+	}
+
+	return false;
+}
+
+//d2 <= d1
+bool Date::operator<=(const Date& d)
+{
+	return *this < d || *this == d;
+}
+
+//d2 > d1
+bool Date::operator>(const Date& d)
+{
+	return !(*this <= d);
+}
+
+//d2 >= d1
+bool Date::operator>=(const Date& d)
+{
+	return !(*this < d);
+}
+
+//d2 == d1
+bool Date::operator==(const Date& d)
+{
+	return _year == d._year
+		&& _month == d._month
+		&& _day == d._day;
+}
+
+//d2 != d1
+bool Date::operator!=(const Date& d)
+{
+	return !(*this == d);
+}
+
+Date Date::operator++(int)
+{
+	Date tmp = *this;
+	*this += 1;
+	return tmp;
+}
+
+Date& Date::operator++()
+{
+	*this += 1;
+	return *this;
+}
+
+Date Date::operator--(int)
+{
+	Date tmp = *this;
+	*this -= 1;
+	return tmp;
+}
+
+Date& Date::operator--()
+{
+	*this -= 1;
+	return *this;
+}
+
+// d1 - d2
+int Date::operator-(const Date& d)
+{
+	int flag = 1;
+	Date max = *this;
+	Date min = d;
+	if (*this < d)
+	{
+		max = d;
+		min = *this;
+		flag = -1;
+	}
+
+	int n = 0;
+	while (min != max)
+	{
+		++min;
+		++n;
+	}
+
+	return n * flag;
 }
