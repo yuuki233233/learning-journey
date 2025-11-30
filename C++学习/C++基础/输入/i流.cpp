ea0fcef5f840bd0a98ea5533789bfacaa4604c1d
Yuuki <yuuki233233@Outlook.com>
2025-11-30 10:01:14 +0800

    输入流

---
+++ C++学习/C++基础/输入/i流.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	double b = 0.1;
+	char c = 'x';
+
+	cout << a << " " << b << " " << c << endl;
+	std::cout << a << " " << b << " " << c << std::endl;
+
+	// 可以⾃动识别变量的类型
+	cin >> a >> b >> c;
+	cout << a << " " << b << " " << c << endl;
+	return 0;
+}
