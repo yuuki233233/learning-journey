3c56a96916d244ee610cec37f80e90823fab6b5e
Yuuki <yuuki233233@Outlook.com>
2025-12-03 18:54:24 +0800

    添加练习代码

---
+++ C++学习/类和对象/拷贝构造函数/省略构造.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+class Date
+{
+public:
+	Date(int year = 1, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+
+	void Print()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+
+	//Date(const Date& d)
+	//{
+	//	_year = d._year;
+	//	_month = d._month;
+	//	_day = d._day;
+	//}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+void Func1(Date& d)
+{
+	cout << &d << endl;
+	d.Print();
+}
+
+int main()
+{
+	Date d1(2025, 11, 16);
+	d1.Print();
+
+	Func1(d1);
+
+	Date d2(d1);
+	d2.Print();
+
+	return 0;
+}
