4ed3d4378279a43c99f1040ead5157bae116bc33
Yuuki <yuuki233233@Outlook.com>
2025-12-07 23:09:13 +0800

    完善贪吃蛇代码

--- 项目/贪吃蛇/snake.h
+++ 项目/贪吃蛇/snake.h
@@ -1,15 +1,19 @@
 #pragma once
+
 #include<stdio.h>
-#include<assert.h>
-#include<stdlib.h>
-#include<locale.h>
-#include<Windows.h>
 #include<stdbool.h>
+#include<Windows.h>
+#include<stdlib.h>
+#include<time.h>
 
-//类型的声明
+#define POS_X 24
+#define POS_Y 5
 
+#define WALL L'□'
+#define BODY L'●'
+#define FOOD L'★'
 
-//蛇的方向
+//方向
 enum DIRECTION
 {
 	UP = 1,
@@ -18,42 +22,78 @@ enum DIRECTION
 	RIGHT
 };
 
-//蛇的状态
-//正常、撞墙、撞到自己、正常退出
+//状态
 enum GAME_STATUS
 {
-	ok,//正常
+	OK,//正常
 	KILL_BY_WALL,//撞墙
-	KILL_BY_SELF,//撞到自己
+	KILL_BY_SELF,//撞自己
 	END_NORMAL//正常退出
 };
 
-//蛇身的节点类型
+//节点
 typedef struct SnakeNode
 {
 	//坐标
 	int x;
 	int y;
 	//指向下一个节点的指针
-	struct SNakeNode* Next;
-}SnakeNode, *pSnakeNode;
-
+	struct SnakeNode* Next;
+}SnakeNode, * pSnakeNode;
 
 //贪吃蛇
-typedef struct Snake
+typedef struct Sanke
 {
-	pSnakeNode _pSnake;//指向蛇头的指针
+	pSnakeNode _pSnake;//指向蛇头的头
+
 	pSnakeNode _pFood;//指向食物节点的指针
-	enum DIRECTION _dir;//蛇的方向
+
+	enum DIRECTION _dir;//方向
+
 	enum GAME_STATUS _status;//游戏的状态
-	int _food_weight;//一个食物的分数
-	int _score;//总成绩
-	int _sleep_time;//休息时间，时间越短，速度越快，时间越长，速度越慢
-}Snake, *pSnake;
 
+	int _food_weight;//食物的分数
 
-//函数的声明
+	int _score;//总分数
+
+	int _sleep_time;//休息时间
+}Snake, * pSnake;
+
+//坐标位置
+void SetPos(short x, short y);
+
+//欢迎界面
 void GameStart(pSnake ps);
 
-//1. 打印欢迎界面
-void WelconetoGame();
+//绘制地图
+void WelcomeToGame();
+
+//创建蛇
+void InitSnake(pSnake ps);
+
+//创建食物
+void CreateFood(pSnake ps);
+
+//游戏运行逻辑
+void GameRun(pSnake ps);
+
+//蛇的移动(走一步)
+void SnakeMove(pSnake ps);
+
+//判断下一个坐标是否是食物
+int NextIsFood(pSnakeNode pn, pSnake ps);
+
+//下一个位置为是食物，就吃掉食物
+void EatFood(pSnakeNode pn, pSnake ps);
+
+//下一个位置不是食物
+void NoFood(pSnakeNode pn, pSnake ps);
+
+//撞到墙
+void KillByWall(pSnake ps);
+
+//撞到自己
+void KillBySelf(pSnake ps);
+
+//结束,游戏善后的工作
+void GameEnd(pSnake ps);
