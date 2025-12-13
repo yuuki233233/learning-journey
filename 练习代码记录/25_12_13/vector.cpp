be80720338a44fc4a291809b4b9da8ad1393b7d5
Yuuki <yuuki233233@Outlook.com>
2025-12-13 19:35:14 +0800

    添加练习代码

--- 练习代码记录/25_12_13/vector.cpp
+++ 练习代码记录/25_12_13/vector.cpp
@@ -1,69 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<vector>
 using namespace std;
 
-namespace yuuki
+void test_vector01()
 {
-	class string
+	vector<int> v1;
+	vector<int> v2(10, 1);
+
+	vector<int> v3(++v2.begin(), --v2.end());
+
+	for (int i = 0; i < v2.size(); ++i)
 	{
-	public:
-		string(const char* str = " ")
-		{
-			_size = strlen(str);
-			_capacity = _size;
-			_str = new char[_capacity + 1];
-			strcpy(_str, str);
-		}
+		cout << v2[i];
+	}
+	cout << endl;
 
-		~string()
-		{
-			delete[] _str;
-			_str = nullptr;
-			_size = _capacity = 0;
-		}
+	for (auto x : v2)
+	{
+		cout << x;
+	}
+	cout << endl;
 
-		void swap(string& s)
-		{
-			std::swap(_str, s._str);
-			std::swap(_capacity, s._capacity);
-			std::swap(_size, s._size);
-		}
-
-		string(const string& s)
-		{
-			string tmp(s._str);
-			swap(tmp);
-		}
-
-		//中间变量
-		string& operator=(const string& s)
-		{
-			if (this != &s)
-			{
-				string tmp(s._str);
-				swap(tmp);
-			}
-			return *this;
-		}
-
-		//直接交换
-		string& operator=(string tmp)
-		{
-			swap(tmp);
-			return *this;
-		}
-
-	private:
-		char* _str;
-		int _size;
-		int _capacity;
-	};
-
+	vector<int>::iterator it = v2.begin();
+	while (it != v2.end())
+	{
+		cout << *it;
+		++it;
+	}
+	cout << endl;
 }
-
 
 int main()
 {
-	
+	test_vector01();
+
 	return 0;
 }
