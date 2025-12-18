7cc80dda292715d8c831fd3896b3a86fd7c9deb2
Yuuki <yuuki233233@Outlook.com>
2025-12-18 16:52:15 +0800

    添加练习代码

--- 练习代码记录/25_12_17/list.h
+++ 练习代码记录/25_12_17/list.h
@@ -6,20 +6,81 @@ namespace yuuki
 {
 	// 节点
 	template<class T>
-	class list_node
+	struct list_node
 	{
 		T _data;
 		list_node<T>* _next;
 		list_node<T>* _prev;
+
+		list_node(const T& data = T()) // 匿名对象
+			:_data(data)
+			,_next(nullptr)
+			,_prev(nullptr)
+		{ }
 	};
 
+	template<class T>
+	struct list_iterator
+	{
+		typedef list_node<T> Node;
+		typedef list_iterator<T> Self;
+		Node* _node;
+
+		list_iterator(Node* node)
+			:_node(node)
+		{ }
+
+		T& operator*()
+		{
+			return _node->_data;
+		}
+
+		Self& operator++()
+		{
+			_node = _node->_next;
+			return *this;
+		}
+
+		Self& operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+
+		bool operator!=(const Self& s) const
+		{
+			return _node != s._node;
+		}
+
+		bool operator==(const Self& s) const
+		{
+			return _node == s._node;
+		}
+	};
 
 	// 结构
 	template<class T>
 	class list
 	{
 		typedef list_node<T> Node;
-	public:
+	public: 
+		typedef list_iterator<T> iterator;
+
+		iterator begin()
+		{
+			/*iterator it(_head->_next); // 有名对象
+			return it;*/
+
+			//return iterator(_head->_next); // 匿名对象
+
+			return _head->_next; // 隐式类型转换
+		}
+
+		iterator end()
+		{
+			return _head;
+		}
+
 		// 构造
 		list()
 		{
@@ -32,13 +93,59 @@ namespace yuuki
 		// 尾插
 		void push_back(const T& x)
 		{
-			Node* newnode = new Node(x);
+			/*Node* newnode = new Node(x);
 			Node* tail = _head->_prev;
 
-			tail->next = newnode;
+			tail->_next = newnode;
 			newnode->_prev = tail;
-			newnode->next = _head;
+			newnode->_next = _head;
 			_head->_prev = newnode;
+
+			++_size;*/
+
+			insert(end(), x);
+		}
+
+		void push_front(const T& x)
+		{
+			insert(begin(), x);
+		}
+
+		void erase(iterator pos)
+		{
+			assert(pos != end());
+
+			Node* prev = pos._node->_prev;
+			Node* next = pos._node->_next;
+
+			prev->_next = next;
+			next->_prev = prev;
+			delete pos._node;
+			--_size;
+		}
+
+		void pop_back()
+		{
+			erase(--end());
+		}
+
+		void pop_front()
+		{
+			erase(begin());
+		}
+
+		void insert(iterator pos, const T& x)
+		{
+			Node* cur = pos._node;
+			Node* prev = cur->_prev;
+
+			Node* newnode = new Node(x);
+
+			// prev newnode cur
+			newnode->_next = cur;
+			cur->_prev = newnode;
+			newnode->_prev = prev;
+			prev->_next = newnode;
 
 			++_size;
 		}
@@ -67,7 +174,13 @@ namespace yuuki
 		lt1.push_back(3);
 		lt1.push_back(4);
 
-
+		list<int>::iterator it = lt1.begin();
+		while (it != lt1.end())
+		{
+			cout << *it << " ";
+			++it;
+		}
+		cout << endl;
 	}
 
 }
